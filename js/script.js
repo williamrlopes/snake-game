3c8c4794d0134336321ca49ce30692b7202f731a
William Lopes <williamlopes3007@gmail.com>
2021-03-03 17:40:15 -0300

    definir movimentos da cobrinha

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ snake[0] = {
     y: 8 * box,
 };
 
+let direction = 'right';
+
 // criando o tabuleiro
 function criarBG() {
     context.fillStyle = 'lightgreen'; /* a cor do tabuleiro */
@@ -29,8 +31,44 @@ function criarCobrinha() {
     }
 }
 
-// chama a função para aparecer no html
-criarBG();
+// Atualizando o jogo de tempos em tempos para que ela consiga se mecher neste intervalo
+// Quando o jogor acabar a função deve parar
+function iniciarJogo() {
+    // chama a função para aparecer no html
+    criarBG();
 
-// chamar função que exibe a cobrinha
-criarCobrinha();
+    // chamar função que exibe a cobrinha
+    criarCobrinha();
+
+    // Definindo a posição inicial (eixo X e Y) da cobrinha
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    // condicional para cada lado que a cobrinha andar
+    if (direcion == 'right') snakeX += box;
+    if (direction == 'left') snakeX -= box;
+    if (direcion == 'up') snakeY -= box;
+    if (direction == 'down') snakeY += box;
+
+    // retirando o último elemento do array
+    snake.pop();
+
+    // acrecentando um box no elemento à frente do início da cobrinha
+    let newhead = {
+        x = snakeX,
+        y = snakeY
+    }
+
+    snake.unshift(newhead);
+
+}
+
+
+
+
+// Passando o intervalo para que a cada 100 milissegundos para a função iniciarJogo
+// Assim o jogo continua sem travar
+// Um intevalo de 100 milisegundos a função iniciarJOgo será reiniciada
+let jogo = setInterval(iniciarJogo, 100);
+
+//Criando as coordenadas da cobrinha (onde ela vai seguir)
